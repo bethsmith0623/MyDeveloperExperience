b70046532c19842e67d539fe3523e0e406aab141
Beth Smith <bethsmith0623@gamil.com>
2019-10-07 11:37:40 -0500

    auth steps 1 & 2

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
 
 module.exports = {
   signup
@@ -8,10 +10,19 @@ async function signup(req, res) {
   const user = new User(req.body);
   try {
     await user.save();
-    // TODO: Send back a JWT instead of the user
-    res.json(user);
+    const token = createJWT(user);
+    res.json({token});
   } catch (err) {
     // Probably a duplicate email
     res.status(400).json(err);
   }
+}
+
+/*---- Helper Functions ----*/
+function createJWT(user) {
+  return jwt.sign(
+    {user},
+    SECRET,
+    {expiresIn: '24h'}
+  );
 }
